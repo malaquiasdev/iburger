033ccbd40fc46a4cbffc5087358d34bfdec8055f
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2022-04-16 11:51:26 -0300

    feat: update more images url in mock

--- src/pages/Menu/Catalog/CatalogItem/index.tsx
+++ src/pages/Menu/Catalog/CatalogItem/index.tsx
@@ -20,11 +20,7 @@ export default function CatalogItem({
   return (
     <div className={styles.item}>
       <div className={styles.item__image}>
-        <img
-          src="https://static-images.ifood.com.br/image/upload/t_low/pratos/8aebf412-c700-4a12-8577-1e4c6b06650d/202012051838_EnlX_.jpeg"
-          alt="logo"
-          className={styles.item__logo}
-        />
+        <img src={imageUrl} alt={title} className={styles.item__logo} />
       </div>
       <div className={styles.item__description}>
         <div className={styles.item__title}>
